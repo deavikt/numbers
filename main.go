b100dbab44b51977df68e51c0c27c6f0c29e7b48
Diana V <deavikt@mail.ru>
2025-06-05 02:44:34 +0400

    handle post method refactoring

--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"numbers/server"
 )
 
-type Number struct {
-	Value int `json:"value"`
-}
-
 func main() {
-	number := Number{
-		Value: 1000,
-	}
-
-	ConvertToJson(number)
-
 	server.Start()
-}
-
-func ConvertToJson(number Number) {
-	data, err := json.Marshal(number)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Printf("%s", data)
 }
--- server/server.go
+++ server/server.go
@@ -3,12 +3,19 @@ package server
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type Number struct {
+	Value int `json:"value"`
+}
 
 const (
 	dataPath string = "/data"
 	port     string = ":8080"
 )
+
+var numbers = make([]Number, 0)
 
 func Start() {
 	http.HandleFunc(dataPath, requestHandler)
@@ -16,31 +23,54 @@ func Start() {
 	err := http.ListenAndServe(port, nil)
 
 	if err != nil {
-		log.Println(err)
-	} else {
-		log.Println("server started at port " + port)
+		log.Println("server startup error: ", err)
 	}
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		getHandler()
+		handleGET()
 	case http.MethodPost:
-		postHandler()
+		handlePOST(r)
 	case http.MethodDelete:
-		deleteHandler()
+		handleDELETE()
+	default:
+		http.Error(w, "This request method isn't supported", http.StatusMethodNotAllowed)
 	}
 }
 
-func getHandler() {
-	log.Println("GET")
+func handleGET() {
+	log.Println("GET request")
 }
 
-func postHandler() {
-	log.Println("POST")
+func handlePOST(r *http.Request) {
+	log.Println("POST request")
+
+	err := r.ParseForm()
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	stringNumber := r.FormValue("value")
+	intNumber, err := strconv.Atoi(stringNumber)
+
+	if err != nil {
+		log.Println("incorrect input")
+	} else {
+		addNumber(intNumber, &numbers)
+	}
 }
 
-func deleteHandler() {
-	log.Println("DELETE")
+func handleDELETE() {
+	log.Println("DELETE request")
+}
+
+func addNumber(number int, numbers *[]Number) {
+	*numbers = append(*numbers, convertIntToNumber(number))
+}
+
+func convertIntToNumber(number int) Number {
+	return Number{Value: number}
 }
